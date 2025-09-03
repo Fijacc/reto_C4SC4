6d66e27be96326a4e8890bc3066be6ffe395710e
Fijacc <jacc.coronado@gmail.com>
2025-09-02 23:59:48 -0600

    Add files via upload

---
+++ reto_C4SC4.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import streamlit as st
+from PIL import Image
+
+employee_data_path = 'Employee_data.csv'
+employee_data = pd.read_csv(employee_data_path)
+
+#1.- Desplegar de un título y una breve descripción #
+
+st.title('Desempeño de los colaboradores del Área de Marketing')
+st.text('Este dashboard web contiene indicadores y filtros del desemenpeño, ' \
+'promedio de horas trabajadas, edad, genero y salario de los colabaoradores ' \
+'del Área de Marketing')
+
+#2.- Desplegar el logotipo de la empresa #
+
+logo = Image.open('Logo.png')
+st.sidebar.image(logo)
+
+############################################################
+
+selected_gender = st.sidebar.multiselect("Selecciona el género del empleado", 
+                                    employee_data['gender'].unique(), default=employee_data['gender'].unique())
+st.sidebar.write(f"Opcion Seleccionada: {selected_gender!r}")
+employee_data_aux = employee_data.loc[employee_data['gender'].isin(selected_gender)]
+
+############################################################
+
+selected_performance = st.sidebar.slider('Selecciona la calificació de desempeño del empleado',
+                                         min_value=float(employee_data['performance_score'].min()),
+                                         max_value=float(employee_data['performance_score'].max()), 
+                                         value= (1.0 ,4.0))
+st.sidebar.write(f"Valores seleccionados: {selected_performance!r}")
+employee_data_aux = employee_data_aux.loc[(employee_data_aux['performance_score']>=selected_performance[0]) & 
+                                          (employee_data_aux['performance_score']<=selected_performance[1])]
+
+############################################################
+
+selected_marital_status = st.sidebar.multiselect("Selecciona el estado civil del empleado", 
+                                                employee_data['marital_status'].unique(), 
+                                                default=employee_data['marital_status'].unique())
+st.sidebar.write(f"Opcion Seleccionada: {selected_marital_status!r}")
+employee_data_aux = employee_data_aux.loc[employee_data['marital_status'].isin(selected_marital_status)]
+
+############################################################
+
+fig = px.histogram(employee_data_aux, x='performance_score', hover_data='performance_score', 
+                   title="Histograma de desempeño")
+st.write(fig)
+
+############################################################
+
+hours_by_gender = employee_data_aux[['gender', 'average_work_hours']].groupby('gender', 
+                                                                          as_index=False).mean()
+fig2 = px.bar(hours_by_gender, x='gender', y='average_work_hours', 
+              title='Horas trabajadas promedio por genero')
+st.write(fig2)
+
+############################################################
+
+salary_by_age = employee_data_aux[['salary', 'age']].groupby('age', 
+                                                         as_index=False).mean()
+fig3 = px.bar(salary_by_age, x='age', y='salary', 
+              title='Horas trabajadas promedio por genero')
+st.write(fig3)
+
+############################################################
+
+fig4 = px.scatter(employee_data_aux, x='average_work_hours', y='performance_score', 
+                  color='gender', title='Horas promedio trabajadas vs desempeño')
+st.write(fig4)
+
+############################################################
+
+
+st.markdown(f'En este analisis se estan considerando **{employee_data_aux['id_employee'].count()}** empleados, ' \
+        f'cuyo promedio de la calificación de su desempeño es: **{employee_data_aux['performance_score'].mean():,.1f}**, ' \
+        f'en promedio trabajan: **{employee_data_aux['average_work_hours'].mean():,.0f}** horas, ' \
+        f'su salario promedio es: **${employee_data_aux['salary'].mean():,.0f}** y ' \
+        f'la edad promedio es : **{employee_data_aux['age'].mean():,.0f}**')
+
+
+# streamlit run "C:\Users\Jorge\Desktop\Data Scientist\Semana 15-16\Streamlit\reto_C4SC4.py"
